ec7c6da3d7d022edafdc08559ad946781c48552c
Matthias Moulin <matthias.moulin@gmail.com>
2019-08-11 19:46:40 +0200

    Update Timing.cpp

--- Timing/Timing/src/Timing.cpp
+++ Timing/Timing/src/Timing.cpp
@@ -1,24 +1,18 @@
 //-----------------------------------------------------------------------------
 // Includes
 //-----------------------------------------------------------------------------
-#pragma region
-
 #include "system\timer.hpp"
-
-#pragma endregion
 
 //-----------------------------------------------------------------------------
 // System Includes
 //-----------------------------------------------------------------------------
-#pragma region
-
+// log
 #include <cmath>
+// cout, endl
 #include <iostream>
 
-#pragma endregion
-
-int main() {
-
+int main()
+{
 	mage::WallClockTimer wall_clock_timer;
 	mage::CPUTimer cpu_timer;
 
@@ -28,7 +22,8 @@ int main() {
 	//  Perform some computation.
 	double sum = 0.0;
 	#pragma omp parallel for schedule(static, 1) reduction(+ : sum)
-	for (int64_t i = 0; i < 10000000000; ++i) {
+	for (int64_t i = 0; i < 10000000000; ++i)
+	{
 		sum += std::log(static_cast< double >(i));
 	}
 
